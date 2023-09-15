c0343a98c02afb79588a591592f8449b3c7816a7
shaneyoung91 <shaneadam.young@gmail.com>
2023-09-15 13:31:35 -0700

    Create KYC Model. Moved Admin code from Main_App to Account

--- account/models.py
+++ account/models.py
@@ -54,7 +54,32 @@ class Account(models.Model):
         
     def __str__(self):
         return f"{self.user}"
+
+class KYC(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=1000)
+    image = models.ImageField(upload_to="kyc", default="default.jpg")
+    marital_status = models.CharField(choices=MARITAL_STATUS, max_length=40)
+    gender = models.CharField(choices=GENDER, max_length=40)
+    identity_type = models.CharField(choices=IDENTITY_TYPE, max_length=140)
+    date_of_birth = models.DateTimeField(auto_now_add=False)
+    signature = models.ImageField(upload_to="kyc")
+
+# Address
+    country = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+
+# Contact Detail
+    mobile = models.CharField(max_length=1000)
+    fax = models.CharField(max_length=1000)
+    date = models.DateTimeField(auto_now_add=True)
     
+    def __str__(self):
+        return f"{self.user}"
+
+
 def create_account(sender, instance, created, **kwargs):
     if created:
         Account.objects.create(user=instance)
@@ -64,3 +89,5 @@ def save_account(sender, instance, **kwargs):
 
 post_save.connect(create_account, sender=User)
 post_save.connect(save_account, sender=User)
+
+
