dadc29953351bdd8555b0ad16aa75b4deeb86252
shaneyoung91 <shaneadam.young@gmail.com>
2023-09-21 10:43:15 -0700

    Create Transaction Model in main_app

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,1 +1,4 @@
 from django.contrib import admin
+from main_app.models import Transaction
+
+admin.site.register(Transaction)
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from userauths.models import User
+from account.models import Account
+from shortuuid.django_fields import ShortUUIDField
 
 # Create your models here.
+
+TRANSACTION_TYPE = (
+    ("Transfer", "Transfer"),
+    ("Received", "Received"),
+    ("Withdraw", "Withdraw"),
+    ("Refund", "Refund"),
+    ("Request", "Request"),
+    ("None", "None"),
+)
+
+TRANSACTION_STATUS = (
+    ("Failed", "Failed"),
+    ("Completed", "Completed"),
+    ("Pending", "Pending"),
+    ("Processing", "Processing"),
+)
+
+class Transaction(models.Model):
+    transaction_id = ShortUUIDField(unique=True, length=15, max_length=20, prefix="TRN")
+    
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="user")
+    amount = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
+    description = models.CharField(max_length=1000, null=True, blank=True)
+    
+    recipient = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="recipient")
+    sender = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="sender")
+    
+    recipient_account = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True, related_name="recipient_account")
+    sender_account = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True, related_name="sender_account")
+    
+    status = models.CharField(choices=TRANSACTION_STATUS, max_length=100, default="Pending")
+    transaction_type = models.CharField(choices=TRANSACTION_TYPE, max_length=100, default="None")
+    
+    date = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    
+    def __str__(self):
+        try:
+            return f"{self.user}"
+        except:
+            return f"Transaction"
