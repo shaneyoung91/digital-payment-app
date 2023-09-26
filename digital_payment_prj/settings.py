d5ffc086f28334b6b3df269422a02fff6439c89f
shaneyoung91 <shaneadam.young@gmail.com>
2023-09-26 15:12:59 -0700

    Update settings.py

--- digital_payment_prj/settings.py
+++ digital_payment_prj/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8@%wgx$kw3z&@z*7s9#)b=j*g+_e__tp+2=_pv^2kaxhf0q+_^'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
