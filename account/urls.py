cf6182844e52395f1011bc9e09f5120de6124b0e
shaneyoung91 <shaneadam.young@gmail.com>
2023-09-27 13:31:54 -0700

    troubleshooting server 500 errors

--- account/urls.py
+++ account/urls.py
@@ -4,7 +4,7 @@ from account import views
 app_name = 'account'
 
 urlpatterns = [
-    path('/', views.account, name='account'),
+    path('', views.account, name='account'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('kyc-reg/', views.kyc_registration, name='kyc-reg'),
 ]
